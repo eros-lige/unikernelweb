2aca2707919cc1e7ee307e4ef8565d27e4fff780
李戈 <geli@sysnew.com>
2016-02-03 17:48:17 +0800

    first commit

---
+++ src/main/java/com/shishuo/cms/exception/PropertyIsNullException.java
@@ -0,0 +1,13 @@
+/*
+ *	Copyright © 2013 Changsha Shishuo Network Technology Co., Ltd. All rights reserved.
+ *	长沙市师说网络科技有限公司 版权所有
+ *	http://www.shishuo.com
+ */
+
+package com.shishuo.cms.exception;
+
+public class PropertyIsNullException extends Exception {
+	public PropertyIsNullException(String msg) {
+		super(msg);
+	}
+}
